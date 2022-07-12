734fdfd54f67ba8a1bf40324e6968bfd55203133
stuartj073 <stuartj073@gmail.com>
2022-07-12 09:56:12 +0000

    Create array to keep all activities on webpage

---
+++ api-practice/src/App.js
@@ -0,0 +1,9 @@
+import logo from './logo.svg';
+import './App.css';
+import Activities from '../../api-project/src/components/Activities';
+
+function App() {
+  return <Activities />
+}
+
+export default App;
--- api-practice/src/components/Activities.js
+++ api-practice/src/components/Activities.js
@@ -1,40 +1,37 @@
 import { useEffect, useState } from 'react';
-// Use effect allows loadActivity to properly run
 
 function Activities() {
     function loadActivity() {
         setIsLoading(true);
-        fetch('http://www.boredapi.com/api/activity/')
+        fetch('http://www.boredapi.com/api/activity')
             .then((response) => response.json())
             .then((data) => {
-                setActivities([...activities, data.activity]);
+                setActivities([...activities, data]);
                 setIsLoading(false);
             });
     }
-    
-    const [activities, setActivities] =  useState([]);
-    const [isLoading, setIsLoading] =  useState(true);
+
+    const [activities, setActivities] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
     useEffect(() => {
         loadActivity()
     }, []);
 
     if(isLoading) {
-        return <p>Loading...</p>
+        <p>Is loading...</p>
     }
-    
-    return  (
+
+    return (
         <div>
             <ul>
                 {activities.map((activity) => {
-                    return <li key={activitiy.key}>{activity.activitiy}</li>
+                    return <li key={activity.key}>{activity.activity}</li>
                 })}
             </ul>
             <h1>{activity}</h1>
-            <button onClick={loadActivity}>Load Another</button>
+            <button onClick={loadActivity}>Load another</button>
         </div>
-    );
+    )
 }
 
 export default Activities;
-
-
