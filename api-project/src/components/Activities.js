7e5d5a901c9f35b6648dc3968d0e178fe6be3827
stuartj073 <stuartj073@gmail.com>
2022-07-12 08:57:45 +0000

    Set up api configuration in activities.js

--- api-project/src/components/Activities.js
+++ api-project/src/components/Activities.js
@@ -1,4 +1,14 @@
+import { useEffect } from 'react';
+
 function Activities() {
+    function loadActivity() {
+        fetch('http://www.boredapi.com/api/activity')
+            .then((response) => response.json())
+            .then((data) => console.log(data));
+    }
+    
+    useEffect(() => {loadActivity});
+    
     return <h1>Something fun</h1>
 }
 
